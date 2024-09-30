d4268546b801a8095e568605a1f18784381a3fd9
jesusinhoo <jesus.bojorquez1110@gmail.com>
2024-09-30 14:08:50 -0700

    Agregar archivos de proyecto.

---
+++ CalcularIMC_JEVB/CalcularIMC_JEVB/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalcularIMC_JEVB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var altura = double.Parse(Altura.Text);
+            var peso = double.Parse(Peso.Text);
+            var imc = peso/(altura*altura);
+
+            IMC.Text = imc.ToString();
+
+            if (imc < 18)
+            {
+                DisplayAlert("Bajo peso","Tienes bajo peso come mas","CERRAR");
+            }
+            else if (imc > 18 && imc <24)
+            {
+                DisplayAlert("Peso normal", "Estas en forma", "CERRAR");
+            }
+            else if (imc > 24 && imc < 29)
+            {
+                DisplayAlert("Sobre peso", "Tienes sobre peso", "CERRAR");
+            }
+            else if (imc > 29)
+            {
+                DisplayAlert("Obecidad", "Tienes obecidad", "CERRAR");
+            }
+        }
+    }
+}
